6dc6ac7b225c4342777cca5efef7a806c4aef3cd
Emily Miles <emiles4@gmail.com>
2018-06-29 04:16:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,6 @@
-# code your #position_taken? method here!
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
 def position_taken?(board)(index)
   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     returns false
+end
 end
